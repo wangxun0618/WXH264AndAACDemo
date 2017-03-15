de8737eca08a347187faa2d6980c2e1fd47a3d37
wangxun <wangxun@topvdn.com>
2017-03-15 13:47:17 +0800

    小修改
    添加注释

--- WXH264Demo/WXH264Demo/Video/VideoDecoder/WXVideoDecoder.h
+++ WXH264Demo/WXH264Demo/Video/VideoDecoder/WXVideoDecoder.h
@@ -13,8 +13,20 @@ typedef void(^VideoDecodeCompleteBlock)(CVPixelBufferRef pixelBuffer);
 
 @interface WXVideoDecoder : NSObject
 
+/**
+ 视频解码
+
+ @param path 视频路径
+ @param complete 完成回调
+ @return 状态码
+ */
 - (WXResult)decodeWithPath:(NSString *)path complete:(VideoDecodeCompleteBlock)complete;
 
+/**
+ 销毁
+
+ @return 状态码
+ */
 - (WXResult)destroy;
 
 @end
